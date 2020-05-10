60272041694227e4e9a56bbc648bb5e77a41d103
Pavan Kalyan Konudula <konudulapavankalyan@gmail.com>
2020-05-10 14:05:48 +0530

    Contact form added

--- index.js
+++ index.js
@@ -12,9 +12,44 @@ function modal(img)
  btn.click();
 }
 
+function formValidation()
+{
+    var uname=document.getElementById('contact_form_name').value;
+  
+    var email=document.getElementById('contact_form_email').value;
+
+    var msg=document.getElementById('contact_form_msg').value;
+    if(uname!="" && msg!="" && email!="")
+    {
+        return true;
+    }
+
+    var error_msg;
+   
+    if(uname=="")
+    {
+        error_msg="Enter your name";
+
+    }
+    else if(email=="")
+    {
+        error_msg="Enter your mail";
+    }
+    else if(msg=="")
+    {
+        error_msg="Message cannot be empty";
+    }
+
+    document.getElementById('error_msg').innerHTML=error_msg;
+
+    return false;
+
+}
 function submitContactForm(e){
 
+    if(formValidation())
     
+    {
   
     var xhr=new XMLHttpRequest();
   
@@ -23,6 +58,9 @@ function submitContactForm(e){
     var email=document.getElementById('contact_form_email').value;
 
     var msg=document.getElementById('contact_form_msg').value;
+
+    
+
 
   
     var pars="uname="+uname+ "&" + "email="+email + "&" + "msg="+msg;
@@ -35,9 +73,14 @@ function submitContactForm(e){
         {
          console.log("Sending");
          var res=this.responseText;
-            console.log(res);
+            if(res=='202')
+            {
+                document.getElementById('error_msg').innerHTML="Your message was sent successfully.";
+            }
          }
       }
       console.log(pars);
       xhr.send(pars);
+    
+    }
   }
