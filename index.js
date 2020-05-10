8be27c420256329610ad5cfc14c8e2620fcbbecd
Pavan Kalyan Konudula <konudulapavankalyan@gmail.com>
2020-05-10 12:53:33 +0530

    Twillo mail api added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+var el = document.getElementById('contact_form_submit');
+if(el){
+  el.addEventListener('click', submitContactForm);
+}
+
+
 
 function modal(img)
 {
@@ -5,3 +11,33 @@ function modal(img)
  document.getElementById('modal_img').src=img;
  btn.click();
 }
+
+function submitContactForm(e){
+
+    
+  
+    var xhr=new XMLHttpRequest();
+  
+    var uname=document.getElementById('contact_form_name').value;
+  
+    var email=document.getElementById('contact_form_email').value;
+
+    var msg=document.getElementById('contact_form_msg').value;
+
+  
+    var pars="uname="+uname+ "&" + "email="+email + "&" + "msg="+msg;
+  
+    xhr.open('POST','https://bbbs0717.000webhostapp.com/sendmail.php',true);
+  
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    xhr.onload=function(){
+       if(this.status == 200)
+        {
+         console.log("Sending");
+         var res=this.responseText;
+            console.log(res);
+         }
+      }
+      console.log(pars);
+      xhr.send(pars);
+  }
